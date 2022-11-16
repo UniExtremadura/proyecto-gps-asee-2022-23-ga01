a1512602b95977ba3f49b6fb839206785ca8d1d2
S4M4R4 <adriannavas091@gmail.com>
2022-11-16 20:13:33 +0100

    PGA01-300 CU11 - IT6 - IMP - Introducido el menu de la app con el cambio de Fragments al seleccionar los distintos iconos del menu, actualmente no funciona el cambio entre los distintos fragments

---
+++ app/src/main/java/es/unex/proyectogps_asee/HomeFragment.java
@@ -0,0 +1,28 @@
+package es.unex.proyectogps_asee;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import es.unex.proyectogps_asee.databinding.FragmentFirstBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
---
+++ app/src/main/java/es/unex/proyectogps_asee/SearchFragment.java
@@ -0,0 +1,28 @@
+package es.unex.proyectogps_asee;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import es.unex.proyectogps_asee.databinding.FragmentFirstBinding;
+
+public class SearchFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
